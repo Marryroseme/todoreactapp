9177e81bcfc00bdeca4e64bce5c37ea8aa673a9f
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-03-18 11:04:40 +0000

    Update description.md

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,5 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you 
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+/
