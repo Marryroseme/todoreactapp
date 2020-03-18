d4c1a6a160469205e262e783228e5380dd8b3a2f
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-03-18 06:43:46 +0000

    [trunk_admin] Update eo_EO, thanks to : sylvbarrier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,4 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
-// It's not ad
+// It's not advisable to add code directly here, but if you 
