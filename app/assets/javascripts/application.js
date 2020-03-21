3b20ad2fba53f779edd2b7a72efc972771242c93
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-03-21 09:43:10 +0000

    Version 0.13.0

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,6 @@
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-/
+// compiled file.
+//
+// Read Sprockets 
