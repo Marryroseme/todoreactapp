5959b1eebf7a13bb0d6a9d1700611a895f5cfdc8
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-04-30 14:29:28 +0000

    [MERGE] from trunk

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,4 +1,4 @@
 class CryptosController < ApplicationController
   before_action :set_crypto, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  bef
+  before_action :correct_user, only: [:show, :edit, 
