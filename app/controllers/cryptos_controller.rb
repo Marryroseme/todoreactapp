8a33b06147354e7a1fe4af9ff58b1e8be7f4cf74
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-04-30 12:13:21 +0000

    addressed PR comments

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,2 +1,4 @@
 class CryptosController < ApplicationController
-  before_action :set_crypto, only: [:show, :edit, :update, :
+  before_action :set_crypto, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  bef
