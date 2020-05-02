6bd2d78e892ae04af8cc8cca651cf5102a0393af
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-05-02 08:34:21 +0000

    fix: README

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,4 +1,6 @@
 class CryptosController < ApplicationController
   before_action :set_crypto, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :correct_user, only: [:show, :edit, 
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  # GET /cryptos
+  # 
