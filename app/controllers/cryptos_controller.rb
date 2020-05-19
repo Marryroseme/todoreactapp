eb9ec3d988536320b25d37884ee20d6ab4cfec7a
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-05-19 22:00:47 +0000

    Run scan-local.sh on thrift:...

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -50,4 +50,4 @@ class CryptosController < ApplicationController
         format.json { render :show, status: :created, location: @crypto }
       else
         format.html { render :new }
-        format.json { render json: @c
+        format.json { render json: @crypto.errors, status: :unprocessa
