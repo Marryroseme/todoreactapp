20cd262814e3006784965de7baec24d794f33ff3
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-05-28 12:07:00 +0000

    Update Territory.html

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -62,4 +62,5 @@ class CryptosController < ApplicationController
       if @crypto.update(crypto_params)
         format.html { redirect_to @crypto, notice: 'Crypto was successfully updated.' }
         format.json { render :show, status: :ok, location: @crypto }
- 
+      else
+        format.html { render :edit }
