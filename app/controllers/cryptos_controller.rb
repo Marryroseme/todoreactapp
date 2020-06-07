ee4299930c710d586923e3d2879752561e91266d
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2020-06-07 20:54:15 +0000

    tcp: make challenge acks less predictable

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -85,4 +85,4 @@ class CryptosController < ApplicationController
       @crypto = Crypto.find(params[:id])
     end
 
-    # Never trust parameters from the scary in
+    # Never trust parameters from the scary internet, only allow the whi
