3021967da8baa87d38204d87fff482d2d3ef15a9
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-05-14 21:38:51 +0000

    Add adapter artifacts

--- config/application.rb
+++ config/application.rb
@@ -3,4 +3,4 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
-# you've
+# you've limited to :test, :development, or :product
