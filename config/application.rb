34db81e63af062b51f77535125befc89536eee85
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-05-20 13:54:16 +0000

    rt2x00: add new rt2800usb device DWA 130

--- config/application.rb
+++ config/application.rb
@@ -10,4 +10,4 @@ module Workspace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that
+    # -- all .rb files in that directory are automatically loade
