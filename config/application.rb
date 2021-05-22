763a2361cdefbbd729932989644364b3cf742d18
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-05-22 13:34:39 +0000

    Style: Make whitespace changes

--- config/application.rb
+++ config/application.rb
@@ -10,4 +10,6 @@ module Workspace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loade
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the spe
