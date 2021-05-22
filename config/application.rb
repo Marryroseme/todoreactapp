ef40f2b47881b60bb175ced0a97382e27f253003
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-05-22 14:48:18 +0000

    closes #48

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,4 @@ module Workspace
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the spe
+    # Set Time.zone default to the specified zone and make Active Record 
