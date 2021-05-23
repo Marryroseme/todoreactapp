7d780d3e2816f8e544ee7f26c692b4d6988c05ce
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-05-23 15:41:04 +0000

    git tracks changes

--- config/application.rb
+++ config/application.rb
@@ -14,4 +14,4 @@ module Workspace
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.ti
+    # config.time_zone = 'Central Time (US & Canada)
