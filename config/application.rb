5ba523848184e4d04d1ab8134a1fd761019591ab
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-05-23 10:40:43 +0000

    qmi_wwan: Add support for HP lt4112 LTE/HSPA+ Gobi 4G Modem

--- config/application.rb
+++ config/application.rb
@@ -13,4 +13,4 @@ module Workspace
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D 
+    # Run "rake -D time" for a list of tasks for finding time zo
