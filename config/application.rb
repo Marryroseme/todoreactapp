669d6199e46b200624f352eed1188514fdb8e8fb
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-05-28 05:32:35 +0000

    removed test

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,5 @@ module Workspace
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load
