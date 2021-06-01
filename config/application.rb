a20895de8ff6ab94c4f0106a329e2fe117cd1707
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-06-01 20:49:37 +0000

    Added scripts.

--- config/application.rb
+++ config/application.rb
@@ -21,4 +21,4 @@ module Workspace
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_
+    config.active_record.raise_in_transactional_callba
