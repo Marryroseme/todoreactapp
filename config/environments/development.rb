d33cd8b87d4e73537e07cb0e19f87c5e2eadc73b
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-06-21 09:43:25 +0000

    Regen jazzy docs [ci skip]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,4 +2,5 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
-  # every request. This slows down respons
+  # every request. This slows down response time but is perfect for development
+  # since
