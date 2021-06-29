fb9f086ac07ea5cd961d0242d9f82425ae03500e
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-06-29 15:44:10 +0000

    testing auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,3 +12,5 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  # Don't care if the mailer ca
