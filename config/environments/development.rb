314531b9f516934edd0afa4e414c23566bf0b354
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-07-27 22:19:19 +0000

    add travis ci support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,4 +31,5 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  # Adds additional error checking when serving assets at 
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly
