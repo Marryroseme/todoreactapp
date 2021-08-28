e44730964723a2886a5ac86ce7cac6df65918af2
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-08-28 15:26:29 +0000

    Added generated changelog mapping

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.a
+Rails.application.config.assets.version = '1.0'
+
+# Add addi
