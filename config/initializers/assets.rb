8dae20cf8d5dcccda04a125fce8547e5d0b466de
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2021-09-11 15:01:01 +0000

    Update version and README

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.ap
+# Rails.application.config.assets.precompi
