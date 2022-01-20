4961045999d3027c6b70408a9927b0480cc28f5e
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-01-20 18:33:32 +0000

    dist (~iotdb)

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about'
   get 'home/search'
-  post "/home/search" =>
+  post "/home/search" => 'home/search'
+  root 'home#index'
+
