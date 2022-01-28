263f3eb1eac22bbd09b6dcc3c710d8743dcdbe6b
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-01-28 18:14:50 +0000

    v5.1

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,4 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes l
+  # See how all your routes lay out with "rake routes"
