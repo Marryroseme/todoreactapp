5c8a899c6536bb0e67627fe3ee10e0a311bf9eec
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-02-13 17:40:23 +0000

    Python 2.6 compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes"
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your sit
