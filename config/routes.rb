67955fc853e2c7b506aa77cab517f9b96118471f
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-02-14 09:35:45 +0000

    v3.0.2

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your sit
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
