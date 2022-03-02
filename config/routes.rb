1f1061b58ac21c478769d0dcfbd5255c4c0b7c41
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-03-02 17:35:08 +0000

    Prepare 3.3.0 release

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
   #   resources :products
 
   # Example resource route with options:
-  #   resources :product
+  #   resources :products do
+  #     member do
+  #   
