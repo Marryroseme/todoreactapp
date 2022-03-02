0d09949e32c498831dbd93dbade89e24145457df
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-03-02 09:45:11 +0000

    clean up logging

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Ex
