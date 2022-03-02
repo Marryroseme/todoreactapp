b7fe980601e8d0886f7da7034a36304f207ebd65
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-03-02 10:37:52 +0000

    USB: gadget: android: Integrate f_midi USB MIDI gadget driver

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs 
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #  
