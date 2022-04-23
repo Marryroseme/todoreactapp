1e47d3e8b8796a2c25d6f41dffb6f264e5e62542
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-04-23 18:26:25 +0000

    updated version info

--- config/routes.rb
+++ config/routes.rb
@@ -58,4 +58,4 @@ Rails.application.routes.draw do
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
-  
+  #     # (app/controllers/admin/products
