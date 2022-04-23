a7a1a36b3979a9d3050d93904f0610db83cc4817
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-04-23 21:49:02 +0000

    merging git tree

--- config/routes.rb
+++ config/routes.rb
@@ -58,4 +58,6 @@ Rails.application.routes.draw do
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
