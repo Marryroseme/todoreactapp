463976483c9eb121c5407cc3aa7fe6d932ca907b
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-07-06 21:44:40 +0000

    Update blog.js

--- db/migrate/20190520132721_devise_create_users.rb
+++ db/migrate/20190520132721_devise_create_users.rb
@@ -4,4 +4,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email, 
+      t.string :email,              null: false, default: ""
+      t.stri
