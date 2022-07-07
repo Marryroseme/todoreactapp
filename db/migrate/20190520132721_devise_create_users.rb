2769b6f1c92c8df78ce36659a952567fe2d1fef8
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-07-07 11:28:34 +0000

    Fix naming convention

--- db/migrate/20190520132721_devise_create_users.rb
+++ db/migrate/20190520132721_devise_create_users.rb
@@ -5,4 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, 
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_
