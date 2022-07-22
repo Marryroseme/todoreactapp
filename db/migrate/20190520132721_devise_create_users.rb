9828a1530d7b34d6b6944f88352e013f24018406
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-07-22 19:49:57 +0000

    android: base-cfg: disable ALARM_DEV

--- db/migrate/20190520132721_devise_create_users.rb
+++ db/migrate/20190520132721_devise_create_users.rb
@@ -25,4 +25,4 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
-  
+      # t.string   :unconfirmed_email # Onl
