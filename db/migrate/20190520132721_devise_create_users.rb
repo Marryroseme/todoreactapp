b62c2eab45026683b2fc62c17319a08f28220ab1
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-07-22 20:35:58 +0000

    Slovak translation update

--- db/migrate/20190520132721_devise_create_users.rb
+++ db/migrate/20190520132721_devise_create_users.rb
@@ -25,4 +25,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Onl
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockab
