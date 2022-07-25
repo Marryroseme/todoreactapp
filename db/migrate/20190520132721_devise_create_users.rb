f2f7f8e01a38050180f250c019e047a1399c8a91
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-07-25 12:31:04 +0000

    Fixed issue #10

--- db/migrate/20190520132721_devise_create_users.rb
+++ db/migrate/20190520132721_devise_create_users.rb
@@ -30,4 +30,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      
+      # t.datetime :locked_at
+
+
+      t.timestam
