35ed096b3a09bd9a9e41233d1d164da872bf6e7f
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-08-04 07:51:19 +0000

    Bump version to 0.1.3.

--- db/migrate/20190520132721_devise_create_users.rb
+++ db/migrate/20190520132721_devise_create_users.rb
@@ -37,4 +37,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, 
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :co
