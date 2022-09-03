47f36b60409248027d85212d58a89bc624c80a0c
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-09-03 10:35:43 +0000

    Add favicon.ico

--- db/schema.rb
+++ db/schema.rb
@@ -4,4 +4,5 @@
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on anothe
+# database schema. If you need to create the application database on another
+# system, you should be using db:schem
