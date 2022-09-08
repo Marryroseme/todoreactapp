a9e61c68cbe6314ac1fb9ee0f2b990be01f4c5a9
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-09-08 19:58:23 +0000

    updated fonts

--- db/schema.rb
+++ db/schema.rb
@@ -11,4 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.defin
+ActiveRecord::Schema.define(version: 20190520162048) do
+
+  crea
