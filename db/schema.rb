50d614838286f78076771ab4331386fefeed42ba
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-09-08 06:58:46 +0000

    Added readme and license.

--- db/schema.rb
+++ db/schema.rb
@@ -5,4 +5,5 @@
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
-# system, you should be using db:schem
+# system, you should be using db:schema:load, not running all the migrations
+# fr
