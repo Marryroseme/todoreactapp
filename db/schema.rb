870dd6e63edcc86c0778ae7291236310beeb3f8e
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-11-23 13:07:46 +0000

    sht3x

--- db/schema.rb
+++ db/schema.rb
@@ -28,4 +28,6 @@ ActiveRecord::Schema.define(version: 20190520162048) do
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
-    t.datetime "reset_password_se
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t
