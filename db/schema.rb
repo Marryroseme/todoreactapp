76585a4c99b59d03f77e96177131666c934ef623
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-12-18 11:13:24 +0000

    release 0.2.6

--- db/schema.rb
+++ db/schema.rb
@@ -35,4 +35,4 @@ ActiveRecord::Schema.define(version: 20190520162048) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_pass
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_p
