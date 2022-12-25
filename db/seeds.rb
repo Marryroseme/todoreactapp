4c909e8357d67a8944f7abe5f277f01410e365f8
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2022-12-25 23:06:43 +0000

    More work done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be 
+# The data can then be loaded with the rake db:seed (or 
