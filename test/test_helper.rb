83fca32cf527edc52b13c5b25073f3c5dc72d5ef
Marryroseme <121728796+Marryroseme@users.noreply.github.com>
2023-02-26 09:59:16 +0000

    workqueue: convert BUG_ON()s in __queue_delayed_work() to WARN_ON_ONCE()s

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,5 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml 
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :a
